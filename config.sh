03ec9c2ee7d91575e690f81823942dad742c2452
An Long <aisk1988@gmail.com>
2021-08-09 19:17:55 +0800

    add test

--- config.sh
+++ config.sh
@@ -10,4 +10,5 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
+    python -c "from lunzi import lunzi; print(lunzi.add())"
 }
